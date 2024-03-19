e1ab1b7d1cef855ad86a83c6dc7b83d4bb9d2f8a
magnomenezes <magnomenezes007@gmail.com>
2024-03-19 18:09:17 -0300

    criação do projeto

---
+++ projeto_java/src/projeto_java/primeira_classe.java
@@ -0,0 +1,10 @@
+package projeto_java;
+
+public class primeira_classe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sou programador JAVA!");
+	}
+
+}
