1611b52b7c091344f979d8d3c2268246c1e9044a
magnomenezes <magnomenezes007@gmail.com>
2024-03-19 18:47:00 -0300

    criacao e alteracao da classe pessoa

--- projeto_java/src/treinogit/pessoa.java
+++ projeto_java/src/treinogit/pessoa.java
@@ -2,9 +2,22 @@ package treinogit;
 
 public class pessoa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
+		private string nome;
+		private string sobrenome;
+		public string getNome() {
+			return nome;
+		}
+		public void setNome(string nome) {
+			this.nome = nome;
+		}
+		public string getSobrenome() {
+			return sobrenome;
+		}
+		public void setSobrenome(string sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+		
+	
 
 }
